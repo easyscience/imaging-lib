79bdb7b684ba6c663bfae6d64a480e56c5d78144
Christian Vedel <christian.vedel@ess.eu>
2026-07-14 16:01:02 +0200

    rename Lattice attributes

--- src/easyimaging/sample_model/lattice.py
+++ src/easyimaging/sample_model/lattice.py
@@ -5,31 +5,45 @@ from easyscience import global_object
 from easyscience.base_classes import ModelBase
 from easyscience.variable import Parameter
 
+Numeric = int | float
+
+
 class Lattice(ModelBase):
     """
     A Lattice represents the periodic arrangement of atoms in a crystal structure, defined by its lattice parameters.
     """
 
-    def __init__(self, a: float, b: float, c: float, alpha: float, beta: float, gamma: float, unique_name=None, display_name=None):
+    def __init__(
+            self,
+            length_a: Numeric,
+            length_b: Numeric,
+            length_c: Numeric,
+            alpha: Numeric,
+            beta: Numeric,
+            gamma: Numeric,
+            unique_name: str | None = None,
+            display_name: str | None = None
+            ):
         """
         Initialize a Lattice instance.
 
         Parameters
         ----------
-        a : float
+        length_a : float | int
             The length of the lattice vector along the x-axis.
-        b : float
+        length_b : float | int
             The length of the lattice vector along the y-axis.
-        c : float
+        length_c : float | int
             The length of the lattice vector along the z-axis.
-        alpha : float
+        alpha : float | int
             The angle between the b and c lattice vectors (in degrees).
-        beta : float
+        beta : float | int
             The angle between the a and c lattice vectors (in degrees).
-        gamma : float
+        gamma : float | int
             The angle between the a and b lattice vectors (in degrees).
         unique_name : str | None
             A unique identifier for the [`Lattice`][..]. Defaults to a generated name if not provided.
         display_name : str | None
             A prettily formatted name for the [`Lattice`][..]. Defaults to [`unique_name`][..unique_name] if not provided.
         """
+
